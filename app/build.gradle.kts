9cea1eedc0d4e7aea57f9b4f7b5482f3e7c86d91
daylyndirkse-blip <daylyndirkse@gmail.com>
2026-09-19 18:08:54 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,4 +36,5 @@ dependencies {
   implementation("androidx.compose.ui:ui-tooling-preview")
   debugImplementation("androidx.compose.ui:ui-tooling")
   implementation("androidx.compose.material3:material3")
+implementation("com.google.android.material:material:1.12.0")
 }
