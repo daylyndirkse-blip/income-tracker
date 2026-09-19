a820fa136f1796d5ee72dff41f221a07027fd953
daylyndirkse-blip <daylyndirkse@gmail.com>
2026-09-19 20:00:35 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,5 @@ plugins {
   id("com.android.application") version "8.5.2" apply false
   id("org.jetbrains.kotlin.android") version "1.9.24" apply false
   id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
+implementation("androidx.core:core-ktx:1.13.1")
 }
